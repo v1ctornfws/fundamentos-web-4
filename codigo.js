ca879a614decf28f5f1371d5b46fa461de82ddaa
Victor Amaya <147742844+v1ctornfws@users.noreply.github.com>
2025-07-03 18:19:54 -0600

    Add files via upload
    error de la logica del codigo original

--- codigo.js
+++ codigo.js
@@ -1,28 +1,28 @@
-lista=[{ 
-      username:"fernando",
-      password:"fer.123",
-      nivel:1  
-    },
-    {
-      username:"raquel",
-      password:"raq.123",
-      nivel:2  
-      }
-  ]
-localStorage.setItem("datos",JSON.stringify(lista));
-usuarios=JSON.parse(localStorage.getItem("datos"));
-console.log(typeof(usuarios));
+lista = [{
+  username: "fernando",
+  password: "fer.123",
+  nivel: 1
+},
+{
+  username: "raquel",
+  password: "raq.123",
+  nivel: 2
+}
+]
+localStorage.setItem("datos", JSON.stringify(lista));
+usuarios = JSON.parse(localStorage.getItem("datos"));
+console.log(typeof (usuarios));
 
 
-function logo(){
+function logo() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  if(username === "fernando" && password === "fer.123"){
+  const validUser = usuarios.find(user => user.username === username && user.password === password);
+  if (!validUser) { return alert('Usuario y/o contraseña incorrectos!') }
+  if (validUser.nivel === 1) {
     window.location.href = "admin.html";
-  } else if(username === "raquel" && password === "raq.123"){
-    window.location.href = "usuario.html";
   } else {
-    alert("Usuario o contraseña incorrectos");
+    window.location.href = "user.html";
   }
 }
